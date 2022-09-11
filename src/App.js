7ce32c73cab45526cca59039b4a744388dbbec66
allan Kiche <allankiche18@gmail.com>
2022-09-12 00:37:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Button, styled } from '@mui/material';
+import AcUnitIcon from '@mui/icons-material/AcUnit'; 
+import {Typography} from '@mui/material';
 function App() {
+
+  const BlueButton  = styled(Button) (({theme}) => ({
+    backgroundColor: theme.palette.otherColor.main,
+  color:"#888",
+  margin: 4,
+  "&: hover":{
+    backgroundColor:"lightblue"
+  },
+  "&:disabled": {
+    backgroundColor: "gray",
+    color: "white",
+  },
+  }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <Button>Primary</Button>
+  <Button disabled>Disabled</Button>
+  <Button variant ="contained" color= "secondary" size ='small"'>Disabled</Button>
+  <Button href="#text-buttons">Link</Button>
+
+  <Button variant="contained">Contained</Button>
+<Button variant="contained" disabled>
+  Disabled
+</Button>
+<Button startIcon ={<AcUnitIcon/>} variant="contained" href="#contained-buttons">
+  Link
+</Button>
+
+<Typography variant="h1" component="h2">
+  h1. Heading
+</Typography>
+<BlueButton>My Button</BlueButton>
+<BlueButton>Another Button</BlueButton>
     </div>
+
+   
   );
 }
 
 export default App;
+
